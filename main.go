dcf715ca462ded44c03eaaadcfe3263950e6205c
Divyanshu Singh <dsinecos@gmail.com>
2019-03-22 18:16:41 +0530

    set response headers

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 type hotdog int
 
 func (h hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Test-Header", "Value for Test Header")
 	fmt.Fprintln(w, "Response from Hotdog handler")
 	fmt.Printf("Method %s \n", req.Method)
 	fmt.Printf("User-Agent %s \n", req.UserAgent())
